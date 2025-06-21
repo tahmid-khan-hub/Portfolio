8574aabbe4ecbca4db1cc5305e838e6a0b69fb2f
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2025-06-21 23:40:21 +0600

    updated navbar, home section and index.css

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,20 +1,30 @@
-import React from 'react';
-import Banner from '../Banner/Banner';
-import About from '../../pages/About/About';
-import Skills from '../../pages/Skills/Skills';
-import Projects from '../../pages/Projects/Projects';
-import Contact from '../../pages/Contact/Contact';
+import React from "react";
+import Banner from "../Banner/Banner";
+import About from "../../pages/About/About";
+import Skills from "../../pages/Skills/Skills";
+import Projects from "../../pages/Projects/Projects";
+import Contact from "../../pages/Contact/Contact";
 
 const Home = () => {
-    return (
-        <div>
-            <Banner></Banner>
-            <About></About>
-            <Skills></Skills>
-            <Projects></Projects>
-            <Contact></Contact>
-        </div>
-    );
+  return (
+    <div>
+      <div id="banner">
+        <Banner />
+      </div>
+      <div id="about">
+        <About />
+      </div>
+      <div id="skills">
+        <Skills />
+      </div>
+      <div id="projects">
+        <Projects />
+      </div>
+      <div id="contact">
+        <Contact />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,26 +1,44 @@
 import React, { useEffect, useState } from "react";
-import { NavLink } from "react-router";
-import { FiSun , FiMoon } from "react-icons/fi";
-import './Navbar.css'
+import { FiSun, FiMoon } from "react-icons/fi";
+import "./Navbar.css";
 
 const Navbar = () => {
+  const [darkMode, setDarkMode] = useState(false);
 
-    const [darkMode, setDarkMode] = useState(false);
+  useEffect(() => {
+    document.body.className = darkMode ? "dark" : "light";
+  }, [darkMode]);
 
-    useEffect(() =>{
-      document.body.className = darkMode ? "dark" : "light"
-    },[darkMode])
-
-    const links = <>
-        <NavLink className="mr-4" to="/"><li>Home</li></NavLink>
-        <NavLink className="mr-4" to="/about"><li>About</li></NavLink>
-        <NavLink className="mr-4" to="/skills"><li>Skills</li></NavLink>
-        <NavLink className="mr-4" to="/projects"><li>Projects</li></NavLink>
-        <NavLink to="/contact"><li>Contact</li></NavLink>
+  const links = (
+    <>
+      <li>
+        <a className="mr-4" href="#banner">
+          Banner
+        </a>
+      </li>
+      <li>
+        <a className="mr-4" href="#about">
+          About
+        </a>
+      </li>
+      <li>
+        <a className="mr-4" href="#skills">
+          Skills
+        </a>
+      </li>
+      <li>
+        <a className="mr-4" href="#projects">
+          Projects
+        </a>
+      </li>
+      <li>
+        <a href="#contact">Contact</a>
+      </li>
     </>
+  );
 
   return (
-    <div className="navbar ">
+    <div className="navbar sticky z-50 top-0 bg-base-300">
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -31,36 +49,35 @@ const Navbar = () => {
               viewBox="0 0 24 24"
               stroke="currentColor"
             >
-              {" "}
               <path
                 strokeLinecap="round"
                 strokeLinejoin="round"
                 strokeWidth="2"
                 d="M4 6h16M4 12h8m-8 6h16"
-              />{" "}
+              />
             </svg>
           </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow "
-          >
+          <ul className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow text-blue-950">
             {links}
           </ul>
         </div>
-        <a className="btn btn-ghost text-xl">Portfolio</a>
+        <a className="text-xl font-semibold ml-3">Portfolio</a>
       </div>
       <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-          {links}
-        </ul>
+        <ul className="menu menu-horizontal px-1">{links}</ul>
       </div>
       <div className="navbar-end">
-        <a onClick={() => setDarkMode(!darkMode)} className="theme-toggle">
-          {
-            darkMode ? <FiSun size={25}></FiSun> : <FiMoon size={25}></FiMoon>
-          }
+        <button onClick={() => setDarkMode(!darkMode)} className="theme-toggle">
+          {darkMode ? <FiSun size={25} /> : <FiMoon size={25} />}
+        </button>
+        <a
+          className="btn ml-2"
+          href="/resume.pdf"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Resume
         </a>
-        <a className="btn ml-2">Resume</a>
       </div>
     </div>
   );
